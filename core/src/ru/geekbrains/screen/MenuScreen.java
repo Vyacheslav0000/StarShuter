3908482a1d0a123c5fd72403a36cd1b6df5cae34
Vyacheslav0000 <slavalol@bk.ru>
2020-03-02 02:47:49 +0300

    Домашнее задание 2

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -6,23 +6,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float V_LEN = 1f;
 
     private Texture img;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 pos;
+    private Texture bg;
 
-    @Override
+    private Logo logo;
+
+    private Background background;
+
+    @ Override
     public void show() {
         super.show();
         img = new Texture("badlogic1.jpg");
-        touch = new Vector2();
-        v = new Vector2();
-        pos = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        logo = new Logo(img);
     }
 
     @Override
@@ -30,27 +34,30 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.5f,0.9f, 0.4f,  1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if ((pos.y + img.getHeight()) < Gdx.graphics.getHeight()){
-            pos.add(v);
-        }
+        logo.update(delta);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         img.dispose();
+        bg.dispose();
         super.dispose();
     }
 
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
+    }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        super.touchDown(screenX, screenY, pointer, button);
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos).setLength(V_LEN));
-        System.out.println("touch.x = " + touch.x + " touch.y " + touch.y);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
